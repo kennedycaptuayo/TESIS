c99fd7270b3be23f3ba934fbd330d67f932f8d1c
captuayo <fg.captuayo@uniandes.edu.co>
2018-04-29 17:04:08 -0500

    Se ajusta vista, se crea nuevas funcionalidades previas al procesamiento

--- angular/src/app/sign-in/sign-in.component.ts
+++ angular/src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroup, FormArray, Validators, FormBuilder} from '@angular/forms';
+import {Office} from '../models/office';
 
 @Component({
   selector: 'app-sign-in',
@@ -8,20 +9,96 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 
 export class SignInComponent implements OnInit {
-  isLinear = true;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  teamForm: FormGroup;
+  departments = [
+    {id: '1', viewValue: 'BOGOTA DC'},
+    {id: '2', viewValue: 'CUNDINAMARCA'},
+    {id: '3', viewValue: 'MEDELLIN'},
+    {id: '4', viewValue: 'BARRANQUILLA'},
+    {id: '5', viewValue: 'CUCUTA'},
+    {id: '6', viewValue: 'SANTA_MARTA'},
+    {id: '7', viewValue: 'BUCARAMANGA'},
+    {id: '8', viewValue: 'CALI'},
+    {id: '9', viewValue: 'PASTO'},
+    {id: '10', viewValue: 'VILLAVICENCIO'},
+    {id: '11', viewValue: 'CARTAGENA'},
+    {id: '12', viewValue: 'ARMENIA'}
+  ];
+  periods_array = [];
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      companyName: ['', Validators.required],
-      cityName: ['', Validators.required]
+    this.createTeamForm();
+    this.addOffice();
+  }
+
+  createTeamForm() {
+    this.teamForm = this.formBuilder.group({
+      company: ['', Validators.required],
+      company: ['', Validators.required],
+      teamManager: '',
+      offices: this.formBuilder.array([]),
+      periods: ['', Validators.required],
+      periodsAdditionalCost: this.formBuilder.array([]),
+      products: this.formBuilder.array([])
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+  }
+
+  get officeFormArray(): FormArray {
+    return this.teamForm.get('offices') as FormArray;
+  }
+
+  addOffice() {
+    const officeFormTemp = this.formBuilder.group({
+      id: [''],
+      name: ['', Validators.required],
+      departament: ['', Validators.required],
     });
+    this.officeFormArray.push(officeFormTemp);
+  }
+
+  get periodsAdditionalCostFormArray(): FormArray {
+    return this.teamForm.get('periodsAdditionalCost') as FormArray;
+  }
+
+  get productsFormArray(): FormArray {
+    return this.teamForm.get('products') as FormArray;
+  }
+
+  addPeriodsAdditionalCost() {
+
+    if (this.periods_array.length === 0) {
+      for (let _i = 0; _i < this.teamForm.get('periods').value; _i++) {
+        this.periods_array.push(_i + 1);
+      }
+    }
+
+    const periodsAdditionalCostFormTemp = this.formBuilder.group({
+      id: ['', Validators.required],
+      cost: ['1.0']
+    });
+
+    this.periodsAdditionalCostFormArray.push(periodsAdditionalCostFormTemp);
+  }
+
+  addProduct() {
+    if (this.periods_array.length === 0) {
+      for (let _i = 0; _i < this.teamForm.get('periods').value; _i++) {
+        this.periods_array.push(_i + 1);
+      }
+    }
+
+    const productsFormTemp = this.formBuilder.group({
+      quantity: [1, Validators.required],
+      name: ['', Validators.required],
+      days: ['', Validators.required]
+    });
+
+    this.productsFormArray.push(productsFormTemp);
+  }
+
+  onFormSubmit() {
   }
 }
